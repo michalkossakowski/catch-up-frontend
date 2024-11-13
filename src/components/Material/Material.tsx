ece8365454944399146498f01eaa6ac36caa5f1f
InterfectoremCubiculum <133600442+InterfectoremCubiculum@users.noreply.github.com>
2024-11-13 21:46:05 +0100

    repaired material

--- src/components/Material/Material.tsx
+++ src/components/Material/Material.tsx
@@ -1,24 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { MaterialDto } from "../../dtos/MaterialDto";
 
-interface MaterialProps {
-    materialId: number;
-    showRemoveFile: boolean;
-    showDownloadFile: boolean;
-    showAddingFile: boolean;
-    materialCreated: (materialId: number) => void;
-  }
+
 const Material = () => {
-
-    const [material, setMaterial] = useState<MaterialDto | null>(null);
-    const [materialName, setMaterialName] = useState<string>('');
-    const [url, setUrl] = useState<string>('');
-    
-    useEffect(() => 
-    {
-        if (materialId !== 0) 
-          getMaterial(materialId);
-    }, 
-    [materialId]);
+    return (<div></div>)
 }
 export default Material;
