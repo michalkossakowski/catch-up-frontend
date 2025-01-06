f21d9098483f0fac8ca5f9908c073cc5e52a5a88
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2025-01-06 16:26:07 +0100

    PT2024OBUE-197 fixed navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import store, { persistor } from './store/store.ts'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './store/store.ts';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import './index.css'
-import App from './App.tsx'
-import AuthProvider from './Provider/authProvider'
+import './index.css';
+import App from './App.tsx';
+import AuthProvider from './Provider/authProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <AuthProvider>
-          <App />
+          <BrowserRouter future={{v7_startTransition: true,v7_relativeSplatPath: true}}> 
+            <App />
+          </BrowserRouter>
         </AuthProvider>
       </PersistGate>
     </Provider>
