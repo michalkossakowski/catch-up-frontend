e65dcb97447489da24b7d69a0e3ca2a9b83bb775
Urszulaak <mala.urszula@gmail.com>
2025-03-17 13:05:05 +0100

    add done/not done logic to feedbacks

--- src/components/Feedback/FeedbackItem.tsx
+++ src/components/Feedback/FeedbackItem.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FeedbackDto } from '../../dtos/FeedbackDto';
 import { ResourceTypeEnum } from '../../Enums/ResourceTypeEnum';
+import { doneFeedback } from '../../services/feedbackService';
+import { useState } from "react";
 
 type FeedbackItemProps = {
     feedback: FeedbackDto;
@@ -12,6 +14,7 @@ const FeedbackItem: React.FC<FeedbackItemProps> = ({ feedback, onDeleteClick }) 
     const truncateDescription = (description: string): string => {
         return description.length > 300 ? description.substring(0, 300) + '...' : description;
     };
+    const [isDone, setIsDone] = useState(feedback.isDone);
 
     return (
         <tr key={feedback.id}>
@@ -21,6 +24,37 @@ const FeedbackItem: React.FC<FeedbackItemProps> = ({ feedback, onDeleteClick }) 
             <td>{new Date(feedback.createdDate).toLocaleDateString()}</td>
             <td>{ResourceTypeEnum[feedback.resourceType]}</td>
             <td>{feedback.resourceName || 'No title'}</td>
+            <td>
+                {isDone ? (
+                    <button
+                        className="btn btn-success"
+                        onClick={async () => {
+                            if (feedback.id !== undefined) {
+                                await doneFeedback(feedback.id);
+                                setIsDone(false);
+                            } else {
+                                console.error("Feedback ID is undefined");
+                            }
+                        }}
+                    >
+                        Done
+                    </button>
+                ) : (
+                    <button
+                        className="btn btn-secondary"
+                        onClick={async () => {
+                            if (feedback.id !== undefined) {
+                                await doneFeedback(feedback.id);
+                                setIsDone(true);
+                            } else {
+                                console.error("Feedback ID is undefined");
+                            }
+                        }}
+                    >
+                        Not done
+                    </button>
+                )}
+            </td>
             <td>
                 {(
                     <button
