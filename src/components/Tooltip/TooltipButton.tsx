632fcf87b44d91f500eb0e7f59a2685cb7245cbf
InterfectoremCubiculum <eryksliwowskic@gmail.com>
2025-04-06 22:28:16 +0200

    -tooltip bug is still on

--- src/components/Tooltip/TooltipButton.tsx
+++ src/components/Tooltip/TooltipButton.tsx
@@ -3,13 +3,12 @@ import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
 interface TooltipButtonProps {
     tooltipText: string;
     onClick: () => void;
-    shouldShow?: boolean;
     variant?: string;
     placement?: "top" | "bottom" | "left" | "right";
     children: React.ReactNode;
 }
 
-const TooltipButton: React.FC<TooltipButtonProps> = ({ tooltipText, onClick, variant, children, placement, shouldShow }) => (
+const TooltipButton: React.FC<TooltipButtonProps> = ({ tooltipText, onClick, variant, children, placement }) => (
   <OverlayTrigger 
     placement="top" 
     overlay={
