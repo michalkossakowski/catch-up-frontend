d7dd389323e78d22bd18db2f19837f4ef69cf010
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2025-05-02 18:02:29 +0200

    hotfix no role

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,10 @@
 import './Home.css';
-import { useTranslation } from "react-i18next";
 
 export default function Home(): React.ReactElement {
-    const { t } = useTranslation();
+
     return(
         <>
             <div className="container">
-                <h2>{t('welcome-in-catchup')}</h2>
                 <h3>Something went wrong, we cannot fetch your role, refresh the page or re-login please.</h3>
             </div>
         </>
