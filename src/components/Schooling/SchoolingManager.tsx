b092b714756e566f3f50079be2f1f250d2e11804
Eryk Śliwowski <eryksliwowskic@gmail.com>
2025-05-26 15:29:48 +0200

    #

---
+++ src/components/Schooling/SchoolingManager.tsx
@@ -0,0 +1,13 @@
+
+
+const SchoolingManager: React.FC = () => {
+
+
+  return (
+    <div className="schooling-manager">
+      <h1>Schooling Manager</h1>
+      <p>This component will manage the schooling process.</p>
+      {/* Additional functionality can be added here */}
+    </div>
+  );
+}
